200fb06f43409d8797b1d96f1deb7a7fb53125cb
Lichuanro <Lichuanro@users.noreply.github.com>
2017-10-01 23:31:30 +0800

    move detection code to separate file

---
+++ detect_smoke.py
@@ -0,0 +1,68 @@
+import numpy as np
+from preprocess_img import resize_img
+import cv2
+
+
+
+def get_prediction(img, model):
+    resized_img = resize_img(img).reshape([1, 224, 224, 3])
+    prediction = model.predict(resized_img)
+    return int(prediction)
+
+def slide_window(img, x_start_stop=[None, None], y_start_stop=[None, None],
+                    xy_window=(64, 64), xy_overlap=(0.5, 0.5)):
+    # If x and/or y start/stop positions not defined, set to image size
+    if x_start_stop[0] == None:
+        x_start_stop[0] = 0
+    if x_start_stop[1] == None:
+        x_start_stop[1] = img.shape[1]
+    if y_start_stop[0] == None:
+        y_start_stop[0] = 0
+    if y_start_stop[1] == None:
+        y_start_stop[1] = img.shape[0]
+    # Compute the span of the region to be searched
+    xspan = x_start_stop[1] - x_start_stop[0]
+    yspan = y_start_stop[1] - y_start_stop[0]
+    # Compute the number of pixels per step in x/y
+    nx_pix_per_step = np.int(xy_window[0]*(1 - xy_overlap[0]))
+    ny_pix_per_step = np.int(xy_window[1]*(1 - xy_overlap[1]))
+    # Compute the number of windows in x/y
+    nx_windows = np.int(xspan/nx_pix_per_step) - 1
+    ny_windows = np.int(yspan/ny_pix_per_step) - 1
+    # Initialize a list to append window positions to
+    window_list = []
+    # Loop through finding x and y window positions
+    for ys in range(ny_windows):
+        for xs in range(nx_windows):
+            # Calculate window position
+            startx = xs*nx_pix_per_step + x_start_stop[0]
+            endx = startx + xy_window[0]
+            starty = ys*ny_pix_per_step + y_start_stop[0]
+            endy = starty + xy_window[1]
+            # Append window position to list
+            window_list.append(((startx, starty), (endx, endy)))
+    # Return the list of windows
+    return window_list
+
+def draw_boxes(img, bboxes, color=(0, 0, 255), thick=6):
+    # Make a copy of the image
+    imcopy = np.copy(img)
+    # Iterate through the bounding boxes
+    for bbox in bboxes:
+        # Draw a rectangle given bbox coordinates
+        cv2.rectangle(imcopy, bbox[0], bbox[1], color, thick)
+    # Return the image copy with boxes drawn
+    return imcopy
+
+def detect_smoke(img, model):
+    # Get all the windows to be searched
+    window_list = slide_window(img)
+    # Will store the suspected smoke region
+    smoke_regions = []
+    for window in window_list:
+        test_img = cv2.resize(img[window[0][1]:window[1][1], window[0][0]:window[1][0]], (64, 64))
+        prediction = get_prediction(test_img, model)
+        if prediction == 1:
+            smoke_regions.append(window)
+    smoke_img = draw_boxes(img, smoke_regions)
+    return smoke_img
